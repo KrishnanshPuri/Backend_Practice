67fd731f0a7f1a0b0635537cbb7be9ce9da114ff
KrishnanshPuri <krishnanshpuri@gmail.com>
2026-06-07 16:26:06 +0530

    fixed errors and set up routes

--- 12)MernAuth/server/server.js
+++ 12)MernAuth/server/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
-
+import authRouter from "./Routes/authRoutes.js";
 const app = expresss();
 const port = process.env.PORT || 3000;
 connectDB();
@@ -11,6 +11,11 @@ connectDB();
 app.use(expresss.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
+
+// API End-Points 
+app.use("/api/auth", authRouter);
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
