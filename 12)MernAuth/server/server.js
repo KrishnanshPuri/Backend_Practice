4ce64c79e224ff74322cd08e96a3ef40caff6d89
KrishnanshPuri <krishnanshpuri@gmail.com>
2026-06-07 15:03:28 +0530

    server started

---
+++ 12)MernAuth/server/server.js
@@ -0,0 +1,17 @@
+import expresss from "express";
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+
+const app = expresss();
+const port = process.env.PORT || 3000;
+connectDB();
+
+app.use(expresss.json());
+app.use(cookieParser());
+app.use(cors({credentials: true}));
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
