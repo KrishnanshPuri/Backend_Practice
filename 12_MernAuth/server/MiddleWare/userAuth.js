b4ea323a75d9d22775093168aa8a54dc1a9d992d
KrishnanshPuri <krishnanshpuri@gmail.com>
2026-06-08 17:22:03 +0530

    error fixed

--- 12_MernAuth/server/MiddleWare/userAuth.js
+++ 12_MernAuth/server/MiddleWare/userAuth.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/userModel.js";
+import User from "../models/userMODEL.js";
 const userAuth = async(req,res,next)=>{
     const {token} = req.cookies;
 
